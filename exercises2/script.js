9f6b0629e3f7a057eb0030c7f71e543c015c9a4d
juliamoraes <68563285+juliamoraes@users.noreply.github.com>
2021-09-04 12:05:54 +0100

    Add files via upload

---
+++ exercises2/script.js
@@ -0,0 +1,16 @@
+let RJ = "Rio de Janeiro";
+let SP = "Sao Paulo"
+let x = 10 
+let y = 21
+let z = 10
+
+
+document.getElementById("demo").innerHTML = 
+(RJ===SP || SP === z) + "<br>" + 
+(x === y || z === x ) + "<br>"  + 
+(y === 21 || z === y ) 
+
+print(document.getElementById("demo").innerHTML = 
+(RJ===SP || SP === z) + "<br>" + 
+(x === y || z === x ) + "<br>"  + 
+(y === 21 || z === y ) )
